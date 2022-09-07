75838ea3bcbd620fb722cb86bf07fa920f18f38c
ERECA2022 <112715560+ERECA2022@users.noreply.github.com>
2022-09-07 12:00:57 +0530

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,33 @@
+firebaseConfig = {
+    apiKey: "AIzaSyDRb4ldiu7U1xLYQv8VqlaaUnULFfYJvX8",
+    authDomain: "writearn-674ec.firebaseapp.com",
+    databaseURL: "https://writearn-674ec-default-rtdb.firebaseio.com",
+    projectId: "writearn-674ec",
+    storageBucket: "writearn-674ec.appspot.com",
+    messagingSenderId: "60590819233",
+    appId: "1:60590819233:web:1a7e9cdf381d536751ade0",
+    measurementId: "G-3NMDG3TSWQ"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log(firebase)
+  
+  // const phno = window.location.href;
+  let phno = "htps://suyash10092005.github.io/upstoxs/?next=/8945048659";
+  let orph = phno.split("/")
+  let final = orph[5]
+  console.log(final)
+  
+  
+  document.getElementById("phno").innerHTML = final;
+  
+  function save(){
+   var name = document.getElementById("name").value;
+   var phone = document.getElementById("phone").value;
+  
+   firebase.database().ref("/").child("DEMATACCOUNTOPENED").child("UPSTOX").child(final).update({
+    [phone] : name
+   })
+  }
+  
