026450fd5ddbdbb45eb2e39f7553fdf7bf038fd0
ERECA2022 <112715560+ERECA2022@users.noreply.github.com>
2022-09-22 18:25:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-firebaseConfig = {
-    apiKey: "AIzaSyDRb4ldiu7U1xLYQv8VqlaaUnULFfYJvX8",
-    authDomain: "writearn-674ec.firebaseapp.com",
-    databaseURL: "https://writearn-674ec-default-rtdb.firebaseio.com",
-    projectId: "writearn-674ec",
-    storageBucket: "writearn-674ec.appspot.com",
-    messagingSenderId: "60590819233",
-    appId: "1:60590819233:web:1a7e9cdf381d536751ade0",
-    measurementId: "G-3NMDG3TSWQ"
-  };
-  
+var firebaseConfig = {
+  apiKey: "AIzaSyDRb4ldiu7U1xLYQv8VqlaaUnULFfYJvX8",
+  authDomain: "writearn-674ec.firebaseapp.com",
+  databaseURL: "https://writearn-674ec-default-rtdb.firebaseio.com",
+  projectId: "writearn-674ec",
+  storageBucket: "writearn-674ec.appspot.com",
+  messagingSenderId: "60590819233",
+  appId: "1:60590819233:web:1a7e9cdf381d536751ade0",
+  measurementId: "G-3NMDG3TSWQ"
+};
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-console.log(firebase)
+// console.log(firebase)
   
-  // const phno = window.location.href;
-  let phno = "htps://suyash10092005.github.io/upstoxs/?next=/8945048659";
+  const phno = window.location.href;
   let orph = phno.split("/")
   let final = orph[5]
-  console.log(final)
+  // console.log(final)
   
   
-  document.getElementById("phno").innerHTML = final;
+  // document.getElementById("phno").innerHTML = final;
   
   function save(){
    var name = document.getElementById("name").value;
-   var phone = document.getElementById("phone").value;
+   var pne = document.getElementById("phone").value;
   
    firebase.database().ref("/").child("DEMATACCOUNTOPENED").child("UPSTOX").child(final).update({
-    [phone] : name
+    [pne] : name
    })
+
+   document.getElementById("name").value = null;
+   document.getElementById("phone").value = null;
+
+   alert("Your Register has been submitted!")
   }
   
